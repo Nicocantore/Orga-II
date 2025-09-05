438f6f8277847421ddbaaacfb7a0b9c65d490bef
Nicocantore <nicocantore123@gmai.com>
2025-09-05 12:16:59 -0300

    Ejercicio 11basica, (para ver si me acuerdo como es el push)

--- C/Guia basica C/11b.c
+++ C/Guia basica C/11b.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 
     int main(){
+        int16_t arry[] = {1,2,3,4};
+        size_t n = sizeof(arry) / sizeof(int16_t);
 
+        int16_t arry2[n];
 
-
+        for (int i = 0 ; i < n; i++){
+            arry2[i] = arry[n-i-1];
+            printf("(%hd) \n",arry2[i]);
+        }
+        
     
 
         return 0;
